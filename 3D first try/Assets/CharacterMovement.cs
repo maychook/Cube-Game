7ac5be07f54bdc6ef630ddc317807b93d5c02466
May Powi <may.powitzer@gmail.com>
2020-04-08 01:30:38 +0300

    Upload Cube Game to git

---
+++ 3D first try/Assets/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float HorizonalMove = 0f;
+    public float VerticalMove = 0f;
+    public float ForwardForce = 500f;
+    public float SidewaysForce = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(0, 30, 30);
+
+        HorizonalMove = Input.GetAxisRaw("Horizontal");
+        VerticalMove = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Vector3 vec = new Vector3(HorizonalMove+ rb.position.x, rb.position.y, VerticalMove + rb.position.z);
+        //rb.MovePosition(vec);
+
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if (Input.GetKey("left"))
+        {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+
+}
---
+++ 3D first try/Assets/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public CharacterMovement movement;
+
+    private void OnCollisionEnter(Collision CollisionInfo)
+    {
+        
+        if (CollisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            //Debug.Log("oops");
+        }
+    }
+}
